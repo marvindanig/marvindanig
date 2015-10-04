dafaed093b36df516a14c737b896ba8bb7bc46a6
Marvin Danig <marvin@marvindanig.com>
2015-10-04 10:35:07 -0400

    Correcting ruby version number

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
 # CONFIGURATION: RBENV
 # ==================
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.2.2p95'
+set :rbenv_ruby, '2.2.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
