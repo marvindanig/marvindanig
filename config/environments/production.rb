3e1b12a6f908b02a0f54024557da70a322880a7f
Marvin Danig <marvin@marvindanig.com>
2015-12-03 12:41:49 -0500

    Updating back to ssl'ed domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Force visitors to HTTPS | Force all access to the app over SSL, use StrictTransportSecurity, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
